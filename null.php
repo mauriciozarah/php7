9b0f1b43e9c1166dbb79f066b4c1803d03f87b28
Mauricio <mzaha@hotmail.com>
2018-07-25 08:59:37 -0300

    PHP 7 operadores

--- null.php
+++ null.php
@@ -7,4 +7,17 @@ $c = 10;
 
 //se $a for null vai para o $b, se $b também for null vai para $c;
 //porem se $b != null para no $b;
-echo $a >> $b >> $c;
+echo $a ?? $b ?? $c;
+
+
+//adiciona a "c" e passa para "a"
+$a = ++$c;
+
+echo "<br> a = " . $a . ", c = " . $c;
+
+//atribui "$c" para "$b" depois soma a $c mais 1
+$b = $c++;
+
+echo "<br> b = " . $b . ", c = " . $c . "<br>";
+
+
