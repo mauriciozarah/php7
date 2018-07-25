e6997ceaff7b862af350585312b5a81343f03b62
Mauricio <mzaha@hotmail.com>
2018-07-25 07:32:05 -0300

    limpando tudo

--- index.php
+++ index.php
@@ -5,15 +5,4 @@ echo "Olá Mundo";
 //TROCANDO VALORES
 //Os valores são trocados com apenas duas variaveis
 
-$a = 7;
-$b = 10;
 
-$a = $a + $b;
-$b = $a - $b;
-$a = $a - $b;
-
-echo "A = " . $a . ", B = " . $b . "<br>";
-
-$vet = [0,1,2,3];
-
-echo $vet[0];
