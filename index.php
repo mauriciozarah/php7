360a9d43a4af57d75ecfc61dd883fbba3f45a129
Mauricio <mzaha@hotmail.com>
2018-07-25 08:20:02 -0300

    space ship

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 
 //porcentagem
-
 $valor = 10;
-
 $valor *= .9;
+//echo $valor;
 
-echo $valor;
+
+//space ship
+
+$a = 50;
+$b = 35;
+
+//se $a for maior traz 1, se for igual traz zero e se $b for maior traz -1
+var_dump($a <=> $b);
