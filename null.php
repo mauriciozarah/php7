3f446143613f591d9c25baf699170d944c5f8412
Mauricio <mzaha@hotmail.com>
2018-07-25 08:30:09 -0300

    Inserido arquivos da aula

---
+++ null.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = null;
+$b = null;
+$c = 10;
+
+
+//se $a for null vai para o $b, se $b também for null vai para $c;
+//porem se $b != null para no $b;
+echo $a >> $b >> $c;
---
+++ space_ship.php
@@ -0,0 +1,17 @@
+<?php
+
+//porcentagem
+$valor = 10;
+$valor *= .9;
+//echo $valor;
+
+
+//space ship
+
+$a = 50;
+$b = 55;
+
+//se $a for maior traz 1, se for igual traz zero e se $b for maior traz -1
+var_dump($a <=> $b);
+
+//null 
